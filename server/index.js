3f8e05131a66dc706907ea3c2a5d8755619d012c
laheravilla <laheravilla@gmail.com>
2019-09-21 04:56:25 +0200

    Connection to socket

--- server/index.js
+++ server/index.js
@@ -3,8 +3,16 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static('client'));
+
 app.get('/', function (req, res) {
     res.status(200).send('Hello World!');
+});
+
+// Abrir conexion con socket
+// Si un cliente se conecta recibimos un aviso en consola
+io.on('connection', function (socket) {
+    console.log('Client with IP' + socket.handshake.address + 'is connected');
 });
 
 server.listen(6677, function () {
